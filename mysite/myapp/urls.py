6e6ae94b5da15469b5afc2731561381c3fdc77f2
shrikanth <shrikanthdeva02@gmail.com>
2022-08-31 11:30:43 +0530

    Added Gunicorn files

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
-
 app_name = 'myapp'
 urlpatterns = [
     path('',views.index),
@@ -19,4 +18,3 @@ urlpatterns = [
     
     
 ]
-
